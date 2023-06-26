dd6e52c812bdadaf9674ecdb61ea4147e18c8c66
Oluwasegun makinde <shegeemankind@gmail.com>
2023-06-26 10:23:13 +0000

    Javascript

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,7 +1,4 @@
-export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
-  expansion1989 === undefined ? expansion1989 = 89 : initialNumber + expansion1989 + expansion2019;
-
-  expansion2019 === undefined ? expansion2019 = 19 : initialNumber + expansion1989 + expansion2019;
-  }
-  
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  return initialNumber + expansion1989 + expansion2019;
 }
+
