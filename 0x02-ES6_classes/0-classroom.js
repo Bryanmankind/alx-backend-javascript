4848189ed8cac85592226d71c47cb74d407acdc8
Oluwasegun makinde <shegeemankind@gmail.com>
2023-06-29 12:16:54 +0000

    ES6 classes

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,4 +1,12 @@
+/** 
+ * Represent a classroom
+ * */
 export default class ClassRoom {
+/**
+   * Creates a new @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - The maximum number of
+   * students in the class.
+   */
   constructor(maxStudentSize) {
     this._maxStudentSize = maxStudentSize;
   }
