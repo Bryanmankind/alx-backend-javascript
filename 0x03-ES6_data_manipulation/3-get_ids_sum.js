bdd87fdca6199ecbf67912c9338195e29eec5b67
Makinde Oluwasegun <shegeemankind@gmail.com>
2023-07-04 16:35:03 +0000

    ES6 Data Manipulation

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,10 +1,12 @@
 /**
- * Retrieves the sum of ids of a list of students.
- */
-export default function getStudentIdsSum(stdId) {
-  if (stdId instanceof Array) {
-const id = stdId.map((a) => a.id);
-return id.reduce((accumilator,currentIndex) => accumilator + currentIndex);
+ * Retrieves the sum of ids of a list of students
+*/
+export default function getStudentIdsSum(students) {
+  if (students instanceof Array) {
+    return students.reduce(
+      (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
+      0,
+    );
   }
   return 0;
 }
