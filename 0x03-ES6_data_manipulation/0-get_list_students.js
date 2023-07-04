c440e34623305afed6a1eb24bd25ce0513cb3d5d
Makinde Oluwasegun <shegeemankind@gmail.com>
2023-07-04 13:12:40 +0000

    ES6 Data Manipulation

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,11 @@
+/**
+ * Retrieves a list of students.
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ */
 export default function getListStudents() {
-  const array = [
-    { id: 1, firstname: "Guillaume", location: "San Francisco" },
-    { id: 2, firstname: "James", location: "Columbia" },
-    { id: 5, firstname: "Serena", location: "San Francisco" },
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
-
-  return array;
 }
